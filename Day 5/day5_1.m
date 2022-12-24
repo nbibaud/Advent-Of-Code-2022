185eabb5825134295e1f394b602089d52b3f8fb7
nbibaud <nbibaud1@gmail.com>
2022-12-24 20:54:58 +0000

    Day 5 part 1

--- Day 5/day5_1.m
+++ Day 5/day5_1.m
@@ -2,16 +2,49 @@
 
 fileID = fopen('input5.txt');
 input = fscanf(fileID, '%c');
-input = string(input);
 fclose(fileID);
 
 
 % parse the boxes nicely :)
 
-    % cut after boxes:
-boxes = split(input," 1   2");
+    % extract data for box stacks:
+boxes = extractBefore(string(input)," 1   2");
     % Separate into lines:
 boxes = string(splitlines(boxes{1}));
     % 9x1 string array, each string representing order of boxes in row:
 boxes = string(char(boxes)');
 boxes = strtrim(boxes(2:4:size(boxes)));
+
+% parse the instructions nicely :)
+
+    % extract instructions data:
+instructions = extractAfter(string(input),"move");
+instructions = erase(instructions, ["move ", "from ", "to "]);
+    % Separate into lines:
+instructions = char(splitlines(instructions{1}));
+instructions = str2num(instructions);
+
+
+% rearrange boxes based on instructions:
+for n = 1:size(instructions,1)
+
+    move = instructions(n,1);
+    from = instructions(n,2);
+    to = instructions(n,3);
+
+    fromStack = char(boxes(from));
+    toStack = boxes(to);
+
+    for m = 1:move
+        toStack = strcat(string(fromStack(1)),toStack);
+        fromStack = fromStack(2:end);
+    end
+
+    boxes(from) = string(fromStack);
+    boxes(to) = string(toStack);
+
+end
+
+% Top crate of each stack:
+boxes = char(boxes);
+solution = (boxes(:,1))'    
