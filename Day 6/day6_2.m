f82c92ff93da4ee38f2bf0e96e4ea3c925f21975
nbibaud <nbibaud1@gmail.com>
2022-12-25 22:22:05 +0000

    Day 6

---
+++ Day 6/day6_2.m
@@ -0,0 +1,19 @@
+% Import data:
+
+fileID = fopen('input6.txt');
+input = fscanf(fileID, '%c');
+fclose(fileID);
+
+% Scan along data stream and stop after marker appears:
+n = 0;
+marker = 0;
+while marker == 0
+    n = n+1;
+    characters = input(n:n+13);
+    if length(characters) == length(unique(characters))
+        marker = 1;
+    end
+end
+
+% Find solution:
+solution = n+13
