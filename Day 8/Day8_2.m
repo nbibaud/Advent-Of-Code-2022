34d0e042b854a374bb076f8872b2e5f173d461e2
nbibaud <nbibaud1@gmail.com>
2022-12-28 01:06:15 +0000

    Day 8 part 2 complete
    yay!

--- Day 8/Day8_2.m
+++ Day 8/Day8_2.m
@@ -1,32 +1,33 @@
-% % Import data:
-% 
-% fileID = fopen('input8.txt');
-% input = fscanf(fileID, '%c');
-% fclose(fileID);
-% 
-% 
-% % Format as numeric matrix:
-% treeGrid = char(splitlines(input))-'0';
+% Import data:
+
+fileID = fopen('input8.txt');
+input = fscanf(fileID, '%c');
+fclose(fileID);
+
+
+% Format as numeric matrix:
+treeGrid = char(splitlines(input))-'0';
 
 
 %---------------------------------------------------------------------------
 
 
 % Create matrices of zeros of same size as tree grid:
-checkHorizontal = zeros(size(treeGrid));
-checkVertical = zeros(size(treeGrid));
+distanceHorizontal = zeros(size(treeGrid));
+distanceVertical = zeros(size(treeGrid));
 
 % Find distance you can see from each tree horizontally and vertically:
 for row = 2:(size(treeGrid,1)-1)
-    checkHorizontal(row,:) = treeView(treeGrid(row,:),checkHorizontal(row,:));
+    distanceHorizontal(row,:) = treeView(treeGrid(row,:),distanceHorizontal(row,:));
 end
 
 for column = 2:(size(treeGrid,2)-1)
-    checkVertical(:,column) = treeView(treeGrid(:,column),checkVertical(:,column));
+    distanceVertical(:,column) = treeView(treeGrid(:,column),distanceVertical(:,column));
 end
 
-% Find number of visible trees overall:
-solution = max(max(checkVertical .* checkHorizontal))
+% Find highest scenic score:
+
+solution = max(max(distanceVertical .* distanceHorizontal))
 
 
 %-----FUNCTIONS----------------------------------
@@ -35,46 +36,43 @@ solution = max(max(checkVertical .* checkHorizontal))
 
 % Function to find visible trees along a line:
 
-function [checkOut] = treeView(line, checkIn)
+function [distance] = treeView(line, distanceIn)
 
-% Looking in one direction (left):
-checkOut1 = checkIn;
-maxTreeHeight = line(1);
+% Looking in one direction:
+distance1 = distanceIn;
     
     for n = 2:numel(line)
-        % If tree is taller than the previous, set value to that of previous tree plus one
-        if line(n) > line(n-1)
-           checkOut1(n) = checkOut1(n-1) + 1;
-        end
-        if line(n) > maxTreeHeight
-            i = n;
-            checkOut1(n) = checkOut1(i) + checkOut1(n-1);
-            maxTreeHeight = line(n);
-        end
-        if line(n) <= line(n-1)
-            checkOut1(n) = 1;
+        view = line(1:n-1);
+
+        % Find distance to closest tree that is higher or same height, or
+        % to edge if there is no higher tree present:
+
+        if sum(view >= line(n)) == 0
+            distance1(n) = n-1;
+        else
+            i = find(view >= line(n));
+            distance1(n) = min(n-i);
         end
     end
 
 % From the other direction:
 line = flip(line);
-maxTreeHeight = line(1);
-checkOut2 = checkIn;
+distance2 = distanceIn;
 
    for n = 2:numel(line)
-        % If tree is taller than the previous, set value to that of previous tree plus one
-        if line(n) > line(n-1)
-           checkOut2(n) = checkOut2(n-1) + 1;
-        end
-        if line(n) > maxTreeHeight
-            i = n;
-            checkOut2(n) = checkOut2(i) + checkOut2(n-1);
-            maxTreeHeight = line(n);
-        end
-        if line(n) <= line(n-1)
-            checkOut2(n) = 1;
-        end
-   end
+        view = line(1:n-1);
 
-   checkOut = checkOut1 .* flip(checkOut2); 
+        % Find distance to closest tree that is higher or same height, or
+        % to edge if there is no higher tree present:
+
+        if sum(view >= line(n)) == 0
+            distance2(n) = n-1;
+        else
+            i = find(view >= line(n));
+            distance2(n) = min(n-i);
+        end
+    end
+
+   distance = distance1 .* flip(distance2); 
+
 end
